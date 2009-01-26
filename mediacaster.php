d0df083ea42f1ecf7b2102eec9e92e7057f94331
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-26 12:45:13 +0000

    fix major compat bug
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/mediacaster/trunk@675 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- mediacaster.php
+++ mediacaster.php
@@ -3,7 +3,7 @@
 Plugin Name: Mediacaster
 Plugin URI: http://www.semiologic.com/software/publishing/mediacaster/
 Description: Lets you add podcasts and videos to your site's posts and pages.
-Version: 1.5.4
+Version: 1.5.5
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 */
@@ -138,7 +138,7 @@ class mediacaster
 					|
 					videocast
 					)\s*
-					(\#[^>#]*)			# the media file
+					\#([^>#]*)			# the media file
 					(?:\#[^>]*)?		# and some junk
 				\s*
 				-->
