1c039abfc951f7b13b9479cfeaacf363d6dbda21
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-01 08:10:11 +0000

    place licensed player directly into the svn
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/mediacaster/trunk@2862 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ mediaplayer-licensed/mediaplayer-licensed.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Name: Licensed JWPlayer
+Version: 4.5
+Copyright LongtailVideo, 2009 - All rights reserved.
+Redistributed with permission by Mesoconcepts, as part of Semiologic Pro.
+*/
+?>
