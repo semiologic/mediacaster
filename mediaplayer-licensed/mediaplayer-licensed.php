754c693117c421dea04d47c24192895b2ec8a168
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-07 09:35:17 +0000

    remove useless file
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/mediacaster/trunk@2978 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- mediaplayer-licensed/mediaplayer-licensed.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
-Name: Licensed JWPlayer
-Version: 4.5
-Copyright LongtailVideo, 2009 - All rights reserved.
-Redistributed with permission by Mesoconcepts, as part of Semiologic Pro.
-*/
-?>
